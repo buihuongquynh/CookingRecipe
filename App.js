5f9cc79fa399dbc291441f34bbaf36e6743f8197
buihuongquynh <buigiangquynh@gmail.com>
2021-10-12 08:09:18 +0700

    createPJ

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  FlatList,
+  TextInput,
+  Button,
+  View,
+  ToastAndroid,
+  SafeAreaView,
+} from 'react-native';
+import axios from 'axios';
+import styles from './App.style';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
---
+++ App.style.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    Expression:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        textAlign:'center',
+    },
+    Input:{
+        borderColor:'#999',
+        borderWidth:1,
+        paddingBottom:2,
+        paddingTop:2,
+        paddingLeft:10,
+        paddingRight:10,
+        margin:10,
+        borderRadius:4,
+    },
+    Kq:{
+        backgroundColor:'#999999',
+        textAlign:'center',
+        paddingBottom:2,
+        paddingTop:2,
+        paddingLeft:10,
+        paddingRight:10,
+    },
+    lbKq:{
+        textAlign:'center'
+    },
+    Item:{
+       borderBottomColor:'#999999',
+       borderBottomWidth:1,
+        marginTop:7,
+      
+        marginBottom:7,   
+    },
+    ListItem:{
+        marginTop:50,
+        marginLeft:5,
+    }
+});
+export default styles;
